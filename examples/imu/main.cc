cde5fb863ea67609b9f0d622372c08b13c350be2
Alvin Sun <alvinsunyixiao@gmail.com>
2019-12-30 10:14:59 +0800

    add imu stuff

---
+++ examples/imu/main.cc
@@ -0,0 +1,29 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2020 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include "main.h"
+#include "spi.h"
+
+void RM_RTOS_Init(void) {
+  uint8_t tx = 0x75 | 0x80;
+  uint8_t rx;
+  HAL_SPI_Transmit(&hspi5, &tx, 1, 55);
+  HAL_SPI_Receive(&hspi5, &rx, 1, 55);
+}
