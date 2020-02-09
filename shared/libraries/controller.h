3de953983a6a30a02b547979622925f2c073077c
Alvin Sun <alvinsunyixiao@gmail.com>
2020-02-09 17:20:55 -0600

    add pid doc

--- shared/libraries/controller.h
+++ shared/libraries/controller.h
@@ -5,10 +5,29 @@
 
 namespace control {
 
+/**
+ * @brief simple PID controller
+ */
 class PIDController {
  public:
+   /**
+    * @brief PID controller constructer
+    *
+    * @param kp proportional gain
+    * @param ki intergral gain
+    * @param kd derivative gain
+    */
   PIDController(float kp, float ki, float kd);
+
+  /**
+   * @brief compute output base on current error
+   *
+   * @param error error of the system, i.e. (target - actual)
+   *
+   * @return output value that could potentially drive the error to 0
+   */
   float ComputeOutput(float error);
+
  private:
   arm_pid_instance_f32 pid_f32_;
 };
