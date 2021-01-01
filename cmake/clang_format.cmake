d45c34988d31c30a545c96b6683d2e60c6f07d8c
Alvin Sun <alvinsunyixiao@gmail.com>
2021-01-01 17:48:32 -0600

    use wrapped clang format

--- cmake/clang_format.cmake
+++ cmake/clang_format.cmake
@@ -22,23 +22,33 @@
 find_program(CLANG_FORMAT_EXE clang-format)
 
 # gather all source code
-file(GLOB_RECURSE ALL_SOURCE_FILES 
+file(GLOB_RECURSE ALL_SOURCE_FILES
     ${CMAKE_SOURCE_DIR}/*.c
     ${CMAKE_SOURCE_DIR}/*.cc
     ${CMAKE_SOURCE_DIR}/*.cpp
     ${CMAKE_SOURCE_DIR}/*.h
     ${CMAKE_SOURCE_DIR}/*.hpp
 )
-# exclude build folder
+# exclude build folder and board CubeMX generated code
 list(FILTER ALL_SOURCE_FILES EXCLUDE REGEX .*/.*build.*/.*)
+list(FILTER ALL_SOURCE_FILES EXCLUDE REGEX .*/boards/.*)
 
 # create formatting helper targets
 if (CLANG_FORMAT_EXE)
-    # format all source codes
+    # download a third party clang format python wrapper
+    set(RUN_CLANG_FORMAT ${CMAKE_BINARY_DIR}/run-clang-format.py)
+    file(DOWNLOAD https://raw.githubusercontent.com/Sarcasm/run-clang-format/master/run-clang-format.py
+         ${RUN_CLANG_FORMAT})
+
+    set(FORMAT_PROG "${CMAKE_SOURCE_DIR}/third_party/run-clang-format.py")
+
+    # format code in place
     add_custom_target(format
-        COMMAND ${CLANG_FORMAT_EXE} -i -style=file ${ALL_SOURCE_FILES})
+        COMMAND python3 ${RUN_CLANG_FORMAT} --clang-format-executable ${CLANG_FORMAT_EXE} -i ${ALL_SOURCE_FILES}
+        DEPENDS ${RUN_CLANG_FORMAT} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
     # check for format violations
     add_custom_target(check-format
-        COMMAND ${CLANG_FORMAT_EXE} -style=file -n -Werror ${ALL_SOURCE_FILES})
+        COMMAND python3 ${RUN_CLANG_FORMAT} --clang-format-executable ${CLANG_FORMAT_EXE} ${ALL_SOURCE_FILES}
+        DEPENDS ${RUN_CLANG_FORMAT} WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 endif ()
