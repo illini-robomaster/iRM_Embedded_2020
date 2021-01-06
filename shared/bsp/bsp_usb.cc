2f469e225549bab4ffcdc37346ead18983b460f9
0-EricZhou-0 <52446536+0-EricZhou-0@users.noreply.github.com>
2021-01-06 00:28:31 -0600

    usb (#14)
    * usb
    
    * buffered usb
    
    * usb refactored
    
    * usb refactored
    
    * Update examples/usb/main.cc
    
    Co-authored-by: Alvin Sun <alvinsunyixiao@gmail.com>
    
    * make friend function for usb callback handlers
    
    * assertion logic added
    
    * usb fix
    
    * usb fix
    
    * usb fix
    
    Co-authored-by: Alvin Sun <alvinsunyixiao@gmail.com>

--- shared/bsp/bsp_usb.cc
+++ shared/bsp/bsp_usb.cc
@@ -20,27 +20,134 @@
 
 #include "bsp_usb.h"
 
-static usb_callback_t rm_usb_rx_callback = NULL;
+#include "usbd_cdc_if.h"
+#include "bsp_error_handler.h"
 
-void usb_register_callback(const usb_callback_t callback) {
-  rm_usb_rx_callback = callback;
+#include "cmsis_os.h"
+
+static bsp::USB* usb = nullptr;
+
+namespace bsp {
+
+USB::USB() :
+    rx_size_(0), rx_pending_(0), rx_write_(nullptr), rx_read_(nullptr),
+    tx_size_(0), tx_pending_(0), tx_write_(nullptr), tx_read_(nullptr) {
+  RM_ASSERT_FALSE(usb, "usb initialized twice");
+  usb = this;
 }
 
-void usb_unregister_callback(void) {
-  rm_usb_rx_callback = NULL;
+USB::~USB() {
+  if (rx_write_)
+    delete[] rx_write_;
+  if (rx_read_)
+    delete[] rx_read_;
+  if (tx_write_)
+    delete[] tx_write_;
+  if (tx_read_)
+    delete[] tx_read_;
+  usb = nullptr;
 }
 
-int32_t usb_transmit(uint8_t *buf, uint32_t len) {
-  uint8_t status = CDC_Transmit_FS(buf, (uint16_t)len);
-  if (status == USBD_OK)
-    return len;
-  else if (status == USBD_BUSY)
-    return -1;
-  else // status == USBD_FAIL (shouldn't get here)
-    return -2;
+void USB::SetupTx(uint32_t tx_buffer_size) {
+  /* usb tx already setup */
+  if (tx_size_ || tx_write_ || tx_read_)
+    return;
+
+  tx_size_ = tx_buffer_size;
+  tx_pending_ = 0;
+  tx_write_ = new uint8_t[tx_buffer_size];
+  tx_read_ = new uint8_t[tx_buffer_size];
 }
 
-void RM_USB_RxCplt_Callback(uint8_t *Buf, uint32_t Len) {
-  if (rm_usb_rx_callback)
-    rm_usb_rx_callback(Buf, Len);
+void USB::SetupRx(uint32_t rx_buffer_size) {
+  /* usb rx already setup */
+  if (rx_size_ || rx_write_ || rx_read_)
+    return;
+
+  rx_size_ = rx_buffer_size;
+  rx_pending_ = 0;
+  rx_write_ = new uint8_t[rx_buffer_size];
+  rx_read_ = new uint8_t[rx_buffer_size];
 }
+
+uint32_t USB::Read(uint8_t** data) {
+  taskENTER_CRITICAL();
+  uint32_t length = rx_pending_;
+  *data = rx_write_;
+  /* swap read / write buffer */
+  uint8_t* tmp = rx_write_;
+  rx_write_ = rx_read_;
+  rx_read_ = tmp;
+  rx_pending_ = 0;
+  taskEXIT_CRITICAL();
+  return length;
+}
+
+uint32_t USB::Write(uint8_t* data, uint32_t length) {
+  taskENTER_CRITICAL();
+  if (length > tx_size_)
+    length = tx_size_;
+  /* try to transmit the data */
+  uint8_t status = CDC_Transmit_FS(data, length);
+  if (status == USBD_BUSY || tx_pending_) {
+    /* usb not available, queuing up data */
+    if (length + tx_pending_ > tx_size_) {
+      length = tx_size_ - tx_pending_;
+      RM_EXPECT_TRUE(1, "usb data transmission truncated");
+    }
+    memcpy(tx_write_ + tx_pending_, data, length);
+    tx_pending_ += length;
+  }
+  taskEXIT_CRITICAL();
+  return length;
+}
+
+void USB::TxCompleteCallback() {
+  uint8_t *tmp;
+  UBaseType_t isrflags = taskENTER_CRITICAL_FROM_ISR();
+  /* check if any data is waiting to be transmitted */
+  if (tx_pending_) {
+    /* swap read / write buffer */
+    tmp = tx_read_;
+    tx_read_ = tx_write_;
+    tx_write_ = tmp;
+    /* initiate new transmission call for pending data */
+    CDC_Transmit_FS(tx_read_, tx_pending_);
+    /* clear the number of pending bytes */
+    tx_pending_ = 0;
+  }
+  taskEXIT_CRITICAL_FROM_ISR(isrflags);
+}
+
+void USB::RxCompleteCallback() {}
+
+uint32_t USB::QueueUpRxData(const uint8_t* data, uint32_t length) {
+  if (length + rx_pending_ > rx_size_) {
+    length = rx_size_ - rx_pending_;
+    RM_EXPECT_TRUE(1, "usb data reception truncated");
+  }
+  memcpy(rx_write_ + rx_pending_, data, length);
+  rx_pending_ += length;
+  return length;
+}
+
+void TxCompleteCallbackWrapper() {
+  if (usb)
+    usb->TxCompleteCallback();
+}
+
+void RxCompleteCallbackWrapper(uint8_t* data, uint32_t length) {
+  usb->QueueUpRxData(data, length);
+  usb->RxCompleteCallback();
+}
+
+} /* namespace bsp */
+
+void RM_USB_TxCplt_Callback() {
+  bsp::TxCompleteCallbackWrapper();
+}
+
+void RM_USB_RxCplt_Callback(uint8_t* data, uint32_t length) {
+  bsp::RxCompleteCallbackWrapper(data, length);
+}
+
