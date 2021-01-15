173bb98855f341ff7fcb5cd9f32dd53905a82a55
Alvin Sun <alvinsunyixiao@gmail.com>
2021-01-15 11:46:10 -0600

    override malloc

--- shared/bsp/bsp_memory.cc
+++ shared/bsp/bsp_memory.cc
@@ -21,6 +21,16 @@
 #include <cstddef>
 #include "cmsis_os.h"
 
+/* overload c memory allocator */
+
+extern "C" void* __wrap_malloc(size_t size) {
+  return pvPortMalloc(size);
+}
+
+extern "C" void __wrap_free(void* ptr) {
+  vPortFree(ptr);
+}
+
 /* overload c++ default dynamic memory allocator */
 
 void* operator new(size_t size) {
